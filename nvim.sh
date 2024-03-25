77c9ad0f91d598d5d2919df742bf322fd4601956
Deadlica <samuelgreenberg00@gmail.com>
2024-03-25 12:11:56 +0100

    updated, renamed script

---
+++ nvim.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+main() {
+  if [ $# -eq 1 ]; then
+    if [ "$1" == "build" ]; then
+      git pull
+      cp -rf ./nvim/lua ~/.config/nvim/
+    else
+      echo -e "Invalid argument.\nUsage: $0 [build | push <commit message>]"
+      exit 1
+    fi
+  elif [ $# -eq 2 ]; then
+    if [ "$1" == "push" ]; then
+      cp -rf ~/.config/nvim ./
+      rm -rf ./nvim/.git/ ./nvim/.github/
+      git add nvim nvim.sh
+      git commit -m "$2"
+      git push
+    else
+      echo -e "Invalid argument.\nUsage: $0 [build | push <commit message>]"
+      exit 1
+    fi
+  else
+    echo -e "Invalid argument.\nUsage: $0 [build | push <commit message>]"
+    exit 1
+  fi
+}
+
+main "$@"
